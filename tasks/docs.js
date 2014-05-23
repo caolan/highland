d44df97b604f37ae11ec0d9c20e70c1dd7138bfb
greelgorke <greelgorke@gmail.com>
2014-05-23 09:20:47 +0200

    Documentation Changes: Navigation and order of items in it - split the Streams section into 4 - move _.nil to Utils - re-order dynamic sections into logical groups - sort items per section alphabetically

--- tasks/docs.js
+++ tasks/docs.js
@@ -8,6 +8,31 @@ var src_file = path.resolve(__dirname, '../lib/index.js');
 var tmpl_file = path.resolve(__dirname, '../docs/templates/base.html');
 var out_file = path.resolve(__dirname, '../docs/index.html');
 
+var sectionsOrder = [ 'Stream Objects',
+                      'Transforms',
+                      'Higher-order Streams',
+                      'Consumption',
+                      'Utils',
+                      'Objects',
+                      'Functions',
+                      'Operators'
+                    ];
+
+function groupBySectionsInSectionsOrder(c1, c2){
+  var idx1 = sectionsOrder.indexOf( c1.section ),
+      idx2 = sectionsOrder.indexOf( c2.section );
+
+  // actially this is just for typo checking
+  if ( idx1 === -1 ) {
+    throw new Error('Invalid Section for entry: ' + c1.id);
+  }
+
+  if ( idx2 === -1 ) {
+    throw new Error('Invalid Section for entry: ' + c1.id);
+  }
+
+  return ( idx1 - idx2 );
+}
 
 module.exports = function (grunt) {
 
@@ -18,7 +43,16 @@ module.exports = function (grunt) {
                 return x.api === 'public';
             });
 
-            var sections = {};
+            comments.sort(groupBySectionsInSectionOrder);
+
+            var sections = {},
+              containsId = function (id) {
+                  return function (x) { return x.id === id; };
+              },
+              contains = function (items, c) {
+                  return items.some(containsId(c.id));
+              };
+
             comments.forEach(function (c) {
                 if (!sections[c.section]) {
                     sections[c.section] = {
@@ -26,18 +60,21 @@ module.exports = function (grunt) {
                         items: []
                     };
                 }
-                var items = sections[c.section].items,
-                    containsId = function (id) {
-                        return function (x) { return x.id === id; };
-                    },
-                    contains = function () {
-                        return items.filter(containsId(c.id)).length > 0;
-                    };
+                var items = sections[c.section].items;
 
                 if (contains(items, c)) {
                     throw new Error('Duplicate id:' + c.id);
                 }
                 items.push(c);
+            });
+
+            // sort the items in the sections?
+            Object.keys(sections).forEach(function(sec){
+              console.log(sections[sec].items)
+              sections[sec].items.sort(function(item1, item2){
+                return (item1.id > item2.id ? 1 : -1);
+              });
+              console.log(sections[sec].items)
             });
 
             var tmpl_src = fs.readFileSync(tmpl_file).toString();
