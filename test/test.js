efef260a823f458133654407bf7b2a52761334c4
Victor Vu <victorvu2001@gmail.com>
2015-08-09 20:12:57 -0600

    Fix some test names.

--- test/test.js
+++ test/test.js
@@ -411,7 +411,7 @@ exports['constructor'] = {
         test.strictEqual(s, _(s));
         test.done();
     },
-    'constructor from Readable stream with next function - issue #303': function (test) {
+    'from Readable stream with next function - issue #303': function (test) {
         var Readable = Stream.Readable;
 
         var rs = new Readable;
@@ -424,17 +424,17 @@ exports['constructor'] = {
             .toArray(this.tester(['a', 'b', 'c'], test));
         test.done();
     },
-    'constructor - throws error for unsupported object': function (test) {
+    'throws error for unsupported object': function (test) {
         test.throws(function () {
             _({}).done(function () {});
         }, Error, 'Object was not a stream, promise, iterator or iterable: object');
         test.done();
     },
-    'constructor from promise': function (test) {
+    'from promise': function (test) {
         _(Promise.resolve(3)).toArray(this.tester([3], test));
         test.done();
     },
-    'constructor from promise - errors': function (test) {
+    'from promise - errors': function (test) {
         test.expect(3);
         var errs = [];
         _(Promise.reject(new Error('boom')))
@@ -448,18 +448,18 @@ exports['constructor'] = {
                 test.done();
             });
     },
-    'constructor from iterator': function (test) {
+    'from iterator': function (test) {
         _(this.createTestIterator([1, 2, 3, 4, 5]))
             .toArray(this.tester([1, 2, 3, 4, 5], test));
         test.done();
     },
-    'constructor from iterator - error': function (test) {
+    'from iterator - error': function (test) {
         _(this.createTestIterator([1, 2, 3, 4, 5], new Error('Error at index 2'))).errors(function (err) {
             test.equals(err.message, 'Error at index 2');
         }).toArray(this.tester([1, 2], test));
         test.done();
     },
-    'constructor from iterator - final return falsy': function (test) {
+    'from iterator - final return falsy': function (test) {
         test.expect(1);
         _(this.createTestIterator([1, 2, 3, 4, 5], void 0, 0)).toArray(this.tester([1, 2, 3, 4, 5, 0], test));
         test.done();
