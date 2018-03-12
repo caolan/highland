c86edbad4a6416371f6715dc9eb8757b642d1027
Victor Vu <victorvu2001@gmail.com>
2018-03-11 21:43:01 -0500

    Backport changes to toNodeStream tests in 3.x.

--- test/test.js
+++ test/test.js
@@ -2046,47 +2046,56 @@ exports.toPromise = {
 
 exports.toNodeStream = {
     'non-object stream of buffer': function (test) {
+        test.expect(1);
         var buf = new Buffer('aaa', 'utf8');
         var s = _.of(buf).toNodeStream();
+        s.on('end', function() {
+            test.done();
+        });
         s.on('data', function(val) {
             test.same(val, buf);
         });
+    },
+    'non-object stream of string': function (test) {
+        test.expect(1);
+        var s = _.of('aaa').toNodeStream({objectMode: true});
         s.on('end', function() {
             test.done();
         });
-    },
-    'non-object stream of string': function (test) {
-        var s = _.of('aaa').toNodeStream({objectMode: true});
         s.on('data', function(val) {
             test.same(val, 'aaa');
         });
-        s.on('end', function() {
-            test.done();
-        });
     },
     'object stream': function (test) {
+        test.expect(1);
         var s = _.of({a: 1}).toNodeStream({objectMode: true});
+        s.on('end', function(val) {
+            test.done();
+        });
         s.on('data', function(val) {
             test.same(val, {a: 1});
         });
+    },
+    'object stream no objectmode': function (test) {
+        test.expect(0);
+        var s = _.of({a: 1}).toNodeStream();
         s.on('end', function(val) {
             test.done();
         });
-    },
-    'object stream no objectmode': function (test) {
-        var s = _.of({a: 1}).toNodeStream();
         s.on('data', function(val) {
             test.ok(false, 'data event should not be fired');
         });
-        s.on('end', function(val) {
-            test.done();
-        });
     },
     'object stream but stream error': function (test) {
+        test.expect(1);
         var err = new Error('ohno');
         var s = _.fromError(err).toNodeStream({objectMode: true});
         s.on('error', function (e) {
             test.same(e, err);
+            test.done();
+        });
+        s.on('data', function (x) {
+            test.ok(false, 'data event should not be fired.');
             test.done();
         });
     }
